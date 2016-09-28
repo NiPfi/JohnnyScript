fa81a0af5179b7588c94c24b6fca3b6129a74406
Nicola Pfister <NiPfi@users.noreply.github.com>
2016-09-28 21:15:58 +0200

    Refactored, added ValidInput test and created output file

--- src/JohnnyScript.java
+++ src/JohnnyScript.java
@@ -1,28 +1,73 @@
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Files;
+import java.util.ResourceBundle;
 
 /**
  * Compiles JohnnyScript (.jns) files to ram files for the Johnny Simulator
  */
 public class JohnnyScript {
 
-    public static void main(String[] args) {
+    private static final String OUTPUT_EXTENSION = ".ram";
+
+    public static void main(String[] args) throws IOException {
+
+        String filename = getFilename(args);
+        Path output = generateOutputfile(filename);
+
+    }
+
+    /**
+     * Generates the output file named like the input file
+     * @param filename Name of the input file to base the output file on (file ending will be stripped)
+     * @return {@link java.nio.file.Path} object for the output file
+     * @throws IOException if filesystem error
+     */
+    private static Path generateOutputfile(String filename) throws IOException {
+        String name = filename.substring(0, filename.indexOf('.'));
+        String outputFile = name + OUTPUT_EXTENSION;
+        Path outputPath = FileSystems.getDefault().getPath(outputFile);
+        Files.createFile(outputPath);
+        return outputPath;
+    }
+
+    /**
+     * Checks the first argument for a filename and returns it if present and readable
+     *
+     * @param args String array of program arguments
+     * @return Filename given to the program
+     * @throws IllegalArgumentException if false argument is given
+     */
+    private static String getFilename(String[] args) {
         String filename;
         try {
             filename = args[0];
         } catch (ArrayIndexOutOfBoundsException e) {
+            String test = "test";
             System.err.println("No argument given.\nUsage: java JohnnyScript filename");
             throw new IllegalArgumentException();
         }
 
+        checkInputFile(filename);
+
+        return filename;
+    }
+
+    /**
+     * Checks if the given filename is for a readable file that can be used to compile
+     *
+     * @param filename String with the name of a file to check
+     * @throws IllegalArgumentException if file for given filename isn't readable
+     */
+    private static void checkInputFile(String filename) {
         Path path = FileSystems.getDefault().getPath(filename);
 
         if (!Files.isReadable(path)) {
             System.err.println("Invalid Filename: " + filename);
             throw new IllegalArgumentException();
         }
-
     }
 
 }
--- test/JohnnyScriptTest.java
+++ test/JohnnyScriptTest.java
@@ -1,9 +1,12 @@
-import org.junit.Assert;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.junit.Assert.*;
 
@@ -14,9 +17,29 @@ public class JohnnyScriptTest {
 
     private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
+
+    // Valid file for testing
+    private final String validFilename = "jUnit";
+
+    private final String validExtension = ".jns";
+    private final String validFile = validFilename + validExtension;
+    private final Path inputPath = FileSystems.getDefault().getPath(validFile);
+
+    private final String validOutputExtension = ".ram";
+    private final String outputFile = validFilename + validOutputExtension;
+    private final Path outputPath = FileSystems.getDefault().getPath(outputFile);
+
     @Before
     public void setUp() throws Exception {
         System.setErr(new PrintStream(errContent));
+
+        Files.createFile(inputPath);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Files.deleteIfExists(inputPath);
+        Files.deleteIfExists(outputPath);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -30,6 +53,16 @@ public class JohnnyScriptTest {
         String filename = "invalid";
         JohnnyScript.main(new String[]{filename});
         assertEquals("Invalid Filename: " + filename, errContent.toString());
+    }
+
+    @Test
+    public void validInput() throws Exception {
+
+        JohnnyScript.main(new String[]{validFile});
+
+        Path outputPath = FileSystems.getDefault().getPath(validFilename + validOutputExtension);
+        assertTrue("Output file not generated or incorrectly named", Files.exists(outputPath));
+
     }
 
 }
